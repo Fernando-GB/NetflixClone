198ffe999c976940ab46f003e1818d3c6cbbf041
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-15 11:01:44 -0300

    Update Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -14,42 +14,42 @@ export default {
             {
                 slug: 'originals',
                 title: 'Originais do Netflix',
-                items: []
+                items: await basicFetch('/discover/tv?with_network=213&api_key=${API_KEY}')
             },
             {
                 slug: 'treding',
                 title: 'Recomendados para Você',
-                items: []
+                items: await basicFetch('/treding/all/week?api_key=${API_KEY}')
             },
             {
                 slug: 'toprated',
                 title: 'Em Alta',
-                items: []
+                items: await basicFetch('/movie/top_rated?api_key=${API_KEY}')
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: []
+                items: await basicFetch('/discovery/movie?with_genres=28&api_key={API_KEY}')
             },
             {
                 slug: 'comedy',
                 title: 'Comedia',
-                items: []
+                items: await basicFetch('/discovery/movie?with_genres=35&api_key={API_KEY}')
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: []
+                items: await basicFetch('/discovery/movie?with_genres=27&api_key={API_KEY}')
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: []
+                items: await basicFetch('/discovery/movie?with_genres=10749&api_key={API_KEY}')
             },
             {
                 slug: 'documentary',
                 title: 'Documentário',
-                items: []
+                items: await basicFetch('/discovery/movie?with_genres=99&api_key={API_KEY}')
             },
         ];
     }
