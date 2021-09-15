4bec69a25908278b3608e781b4f0df526826bcd4
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-15 10:35:53 -0300

    Update Tmdb.js

---
+++ src/Tmdb.js
@@ -0,0 +1,8 @@
+const API_KEY = 'd6f5da42285fa3077674344a42a83220'
+const API_BASE = 'https://api.themoviedb.org/3';
+
+export default {
+    getHomeList: async () => {
+        return [];
+    }
+}
