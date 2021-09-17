9d461342a9a8a90e889a027814bc30a021024ebf
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-17 12:38:22 -0300

    Update MovieRow.js

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './MovieRow.css';
 
-export default () => {
+export default ({title, items}) => {
     return(
         <div>
-            
+            <h2>{title}</h2>
+            <div className="movieRow--listarea">
+                {items.results.lenght > 0 && items.map((item, key)=>(
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                ))}
+            </div>
         </div>
     );
 }
