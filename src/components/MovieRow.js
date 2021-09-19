125b96d65a1ddbd0479f272f87932f3bc47dafcb
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-19 15:16:31 -0300

    Update MovieRow.js

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './MovieRow.css';
 
+// eslint-disable-next-line
 export default ({title, items}) => {
     return(
         <div>
             <h2>{title}</h2>
             <div className="movieRow--listarea">
-                {items.results.lenght > 0 && items.map((item, key)=>(
+                {items.results.lenght > 0 && items.results.map((item, key)=>(
                     <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
                 ))}
             </div>
