57e35012f8199d532d3a93e013121831bd95d2d1
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-20 12:24:23 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 
-
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
 
-  useEffect (()=>{
+  useEffect(()=>{
     const loadAll = async () => {
 
       let list = await Tmdb.getHomeList();
@@ -20,11 +19,11 @@ export default () => {
 
   return (
     <div className="page">
-        <section className="lists">
-          {movieList.map((item, key)=>(
-           <MovieRow key={key} title={item.title} items={item.items} />
-          ))}
-        </section>
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
