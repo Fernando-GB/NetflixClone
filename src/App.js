a3716548b7fce0c2937b3aefae71c4d8abb5f71e
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-21 13:15:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
