7455bd8b31c447795fca1619196a88fcc371c320
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-23 11:50:11 -0300

    Update FeaturedMovie.js

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,8 +3,12 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
     return (
-        <section class name="featured">
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
             <div>{item.original_name}</div>
-        <section/>
+        </section>
     );
-}     
+}
