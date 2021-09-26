547f16eb7c2b61b94e62b07f79491fd82224ac66
Fernando Guimarães <71611477+Fernando-GB@users.noreply.github.com>
2021-09-26 10:59:07 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,17 @@ import './Header.css';
 
 export default () => {
     return (
-        <header>
-            ...
+        <header className="black">
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="Netflix" />
+                </a>
+            </div>  
+            <div className="header--user">
+                <a href="/">
+                    <img scr="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="User" />
+                </a>
+            </div>      
         </header>
     );
 }
